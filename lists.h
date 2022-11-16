56faaaf68973e36525db7afec7cad5c09db1158c
Ksiren <114107580+Ksiren@users.noreply.github.com>
2022-11-16 22:17:23 +0300

    we can add nums to the head of list
    +Adding an item to the top of the list

--- lists.h
+++ lists.h
@@ -1,63 +1,81 @@
 #include<iostream>
 using namespace std;
 
-struct Node{
+struct Node {
     int data = 0;
-    Node *next = nullptr;
-    
-    ~Node(){
+    Node* next = nullptr;
+
+    ~Node() {
         delete next;
     }
 };
 
-class List{
-    public:
+class List {
+public:
     int size = 0;
-    Node *first = nullptr;
-    
-    public:
-    
-    
-    bool is_empty() const{
-        if (first == nullptr){
+    Node* first = nullptr;
+
+public:
+
+
+    bool is_empty() const {
+        if (first == nullptr) {
             return true;
-        } 
+        }
         return false;
     }
-    
-    void push_back(int num){
-        if (is_empty()){
+
+    void push_back(int num) {
+        if (is_empty()) {
             first = new Node;
-            first -> data = num;
-            size ++;
+            first->data = num;
+            size++;
             return;
-        } else{
-            Node *it = first;
-            while (it->next != nullptr){
+        }
+        else {
+            Node* it = new Node;
+            it = first;
+            while (it->next != nullptr) {
                 it = it->next;
             }
             it->next = new Node;
             it->next->data = num;
-            size ++;
+            // cout << num << ' ' << it->next->data << ' ' << it->data << endl;
+            size++;
         }
     }
-    
-    friend ostream& operator<<(ostream&, const List&);
-    
-    ~List(){
-            delete first;
+
+    void push_ahead(int num) {
+        if (is_empty()) {
+            first = new Node;
+            first->data = num;
+            size++;
+            return;
         }
+        Node* it = new Node;
+        it->data = num;
+        it->next = first;
+        first = it;
+        size++;
+    }
+
+    friend ostream& operator<<(ostream&, const List&);
+
+    ~List() {
+        delete first;
+    }
 };
 
 ostream& operator<<(ostream& out, const List& conc) {
-    Node *it = conc.first;
-    out << "Your list:\n";
-    while (it->next != nullptr){
+    Node* it = new Node;
+    it = conc.first;
+    out << "\nYour list:\n";
+    while (it->next != nullptr) {
         out << it->data << " - ";
         it = it->next;
     }
-    out << it->data; 
-    delete it;
+    out << it->data;
+    // delete it;
     out << "\nlength = " << conc.size << endl;
     return out;
 }
